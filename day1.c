96a3709dbf1e1e49e9bdb06330db5fd9cdcf1813
Ryan Hamilton <rhamil4@gmu.edu>
2023-12-03 08:15:09 -0500

    example solution impl

--- day1.c
+++ day1.c
@@ -2,25 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char result[3] = "  \0";
-    char str[] = "l1t3r4l";
-    int i = 0;
-
+int extract(char str[]) {
+    int i = 0, first, last;
+    
     for (i = 0; i < strlen(str); ++i) {
         if (isdigit(str[i])) {
-            result[0] = str[i];
+            first = str[i] - '0';
             break;
         }
     }
 
     for (i = strlen(str) - 1; i >= 0; --i) {
         if (isdigit(str[i])) {
-            result[1] = str[i];
+            last = str[i] - '0';
             break;
         }
     }
 
-    printf("%s", result);
+    return first * 10 + last;
+}
+
+int main() {
+    char line1[] = "1abc2";
+    char line2[] = "pqr3stu8vwx";
+    char line3[] = "a1b2c3d4e5f";
+    char line4[] = "treb7uchet";
+    int result = extract(line1) + extract(line2) + extract(line3) + extract(line4);
+
+    printf("answer %d", result);
+
     return 0;
 }
