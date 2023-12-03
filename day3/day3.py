252f577bdebd99249b1604b2d5d909afa606c978
Ryan Hamilton <rhamil4@gmu.edu>
2023-12-03 12:28:32 -0500

    resolved scope

--- day3/day3.py
+++ day3/day3.py
@@ -1,35 +1,50 @@
-file = open("C:/Users/rlham/aoc/2023/day3/input.txt", "r")
-prompt = [line[:-1] for line in file]
-file.close()
+def read_prompt():
+    file = open("C:/Users/rlham/aoc/2023/day3/input.txt", "r")
+    prompt = [line[:-1] for line in file]
+    file.close()
+    return prompt
 
-numbers = {}
-symbols = {}
-for i in range(len(prompt)):
-    line = prompt[i]
+def parse_prompt(prompt):
+    numbers = {}
+    symbols = {}
+    for i in range(len(prompt)):
+        line = prompt[i]
 
-    # entry(index: number)
-    nums = {}
-    syms = []
-    number = ''
-    for j in range(len(line)):
-        char = line[j]
-        if char.isdigit(): number += char
-        # if number isn't length 0
-        else:
-            if len(number):
-                nums[j - len(number)] = number
-                number = ''
-            if char != '.': syms.append(j)
-
-    # handle hanging number
-    if len(number):
-        nums[len(line) - len(number)] = number
+        # entry(index: number)
+        nums = {}
+        syms = []
         number = ''
+        for j in range(len(line)):
+            char = line[j]
+            if char.isdigit(): number += char
+            # if number isn't length 0
+            else:
+                if len(number):
+                    nums[j - len(number)] = number
+                    number = ''
+                if char != '.': syms.append(j)
 
-    # if dictionary isn't empty
-    if nums: numbers[i] = nums
-    if syms: symbols[i] = syms
+        # handle hanging number
+        if len(number):
+            nums[len(line) - len(number)] = number
+            number = ''
 
-print(numbers)
-print(symbols)
+        # if not empty
+        if nums: numbers[i] = nums
+        if syms: symbols[i] = syms
 
+    return numbers, symbols
+
+if __name__ == '__main__':
+    prompt = read_prompt()
+    numbers, symbols = parse_prompt(prompt)
+
+    for line, nums in numbers.items():
+        for index, number in nums.items():
+            print(f'on line {line} at index {index} lives number {number}')
+
+    print()
+
+    for line, indices in symbols.items():
+        for i in indices:
+            print(f'on line {line} at index {i} lives a symbol')
