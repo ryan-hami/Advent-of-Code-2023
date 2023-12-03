cb4fd9ad54daf55486166f47b44aef8a5d4474a9
Ryan Hamilton <rhamil4@gmu.edu>
2023-12-03 11:25:33 -0500

    game number can be greater than 9

--- day2/DayTwo.java
+++ day2/DayTwo.java
@@ -20,8 +20,9 @@ public class DayTwo {
         Scanner scnr = new Scanner(new File("C:\\Users\\rlham\\aoc\\2023\\day2\\input.txt"));
         lines: while (scnr.hasNextLine()) {
             String game = scnr.nextLine();
-            int id = Integer.parseInt(String.valueOf(game.charAt(5)));
-            game = game.substring(7);
+            int colonIndex = game.indexOf(":");
+            int id = Integer.parseInt(game.substring(game.indexOf(" ") + 1, colonIndex));
+            game = game.substring(colonIndex + 1);
             String[] sets = game.split(";");
             for (String set : sets) {
                 String[] entries = set.split(",");
@@ -34,6 +35,7 @@ public class DayTwo {
             }
             sum += id;
         }
+
         System.out.println(sum);
     }
 }
