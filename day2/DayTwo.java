eef00a2d0454a0796330446c6a9471653b175445
Ryan Hamilton <rhamil4@gmu.edu>
2023-12-03 11:34:49 -0500

    part 2

--- day2/DayTwo.java
+++ day2/DayTwo.java
@@ -13,12 +13,13 @@ public class DayTwo {
         };
     }
 
-    public static void main(String... args) throws FileNotFoundException {
-        int[] rgb = {12, 13, 14};
+    static void part1(Integer... rgb) throws FileNotFoundException {
         int sum = 0;
 
         Scanner scnr = new Scanner(new File("C:\\Users\\rlham\\aoc\\2023\\day2\\input.txt"));
         lines: while (scnr.hasNextLine()) {
+            int[] min = {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE};
+
             String game = scnr.nextLine();
             int colonIndex = game.indexOf(":");
             int id = Integer.parseInt(game.substring(game.indexOf(" ") + 1, colonIndex));
@@ -37,5 +38,38 @@ public class DayTwo {
         }
 
         System.out.println(sum);
+    }
+
+    static void part2() throws FileNotFoundException {
+        int sum = 0;
+
+        Scanner scnr = new Scanner(new File("C:\\Users\\rlham\\aoc\\2023\\day2\\input.txt"));
+        lines: while (scnr.hasNextLine()) {
+            int[] max = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+
+            String game = scnr.nextLine();
+            int colonIndex = game.indexOf(":");
+            int id = Integer.parseInt(game.substring(game.indexOf(" ") + 1, colonIndex));
+            game = game.substring(colonIndex + 1);
+            String[] sets = game.split(";");
+            for (String set : sets) {
+                String[] entries = set.split(",");
+                for (String entry : entries) {
+                    String[] values = entry.substring(1).split(" ");
+                    int num = Integer.parseInt(values[0]);
+                    int index = indexMap(values[1]);
+                    max[index] = Math.max(num, max[index]);
+                }
+            }
+            int power = max[0] * max[1] * max[2];
+            sum += power;
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void main(String... args) throws FileNotFoundException {
+        part1(12, 13, 14);
+        part2();
     }
 }
