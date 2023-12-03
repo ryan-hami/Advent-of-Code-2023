b2ff78b56954dad692e6eab0ff22ded1c46eb5a7
Ryan Hamilton <rhamil4@gmu.edu>
2023-12-03 09:48:41 -0500

    in-lined initialization

--- day1.c
+++ day1.c
@@ -23,8 +23,7 @@ int evaluate(int i, char str[]) {
         return str[i] - '0';
     }
 
-    val = 0;
-    for (j = i - 2; val == 0 && j >= 0 && j > i - 5; --j) {
+    for (j = i - 2, val = 0; val == 0 && j >= 0 && j > i - 5; --j) {
         char *substr = calloc(1, &str[i + 1] - &str[j] + 1);
         memcpy(substr, &str[j], &str[i + 1] - &str[j]);
         val = digitWordMap(substr);
@@ -34,22 +33,20 @@ int evaluate(int i, char str[]) {
 }
 
 int getFirst(char str[]) {
-    int i, val = 0;
+    int i, val;
 
-    for (i = 0; i < strlen(str); ++i) {
-        int val = evaluate(i, str);
-        if (val > 0) return val;
+    for (i = 0, val = 0; val == 0 && i < strlen(str); ++i) {
+        val = evaluate(i, str);
     }
 
     return val;
 }
 
 int getLast(char str[]) {
-    int i, val = 0;
+    int i, val;
 
-    for (i = strlen(str) - 1; i >= 0; --i) {
-        int val = evaluate(i, str);
-        if (val > 0) return val;
+    for (i = strlen(str) - 1, val = 0; val == 0 && i >= 0; --i) {
+        val = evaluate(i, str);
     }
 
     return val;
@@ -60,9 +57,9 @@ int extract(char str[]) {
 }
 
 int main(int argc, char** argv) {
-    int result = 0;
+    int result;
 
-    for (--argc ; argc > 0; --argc) {
+    for (--argc, result = 0; argc > 0; --argc) {
         result += extract(argv[argc]);
     }
 
