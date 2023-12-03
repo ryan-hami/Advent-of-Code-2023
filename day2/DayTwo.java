5ca9505d79fdceba26fa0c0ad05be9fc54c35bda
Ryan Hamilton <rhamil4@gmu.edu>
2023-12-03 11:14:47 -0500

    fast and dirty solution

--- day2/DayTwo.java
+++ day2/DayTwo.java
@@ -1,16 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 @SuppressWarnings("all")
 public class DayTwo {
+    static int indexMap(String color) {
+        return switch (color) {
+            case "red" -> 0;
+            case "green" -> 1;
+            case "blue" -> 2;
+            default -> -1;
+        };
+    }
+
     public static void main(String... args) throws FileNotFoundException {
-        //Map<Integer, String> map = new HashMap<>();
+        int[] rgb = {12, 13, 14};
+        int sum = 0;
 
         Scanner scnr = new Scanner(new File("C:\\Users\\rlham\\aoc\\2023\\day2\\input.txt"));
-        while (scnr.hasNextLine()) {
+        lines: while (scnr.hasNextLine()) {
             String game = scnr.nextLine();
             int id = Integer.parseInt(String.valueOf(game.charAt(5)));
             game = game.substring(7);
@@ -19,11 +27,13 @@ public class DayTwo {
                 String[] entries = set.split(",");
                 for (String entry : entries) {
                     String[] values = entry.substring(1).split(" ");
-                    int num = Integer.parseInt(values[0]);
-                    String color = values[1];
-                    System.out.println(num + " of color " + color);
+                    if (Integer.parseInt(values[0]) > rgb[indexMap(values[1])]) {
+                        continue lines;
+                    }
                 }
             }
+            sum += id;
         }
+        System.out.println(sum);
     }
 }
